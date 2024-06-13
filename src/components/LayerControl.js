5f4c938ed79ffd8af8d1f08256f340d973f06a70
Anastasia <ndthwm@yandex.ru>
2024-06-13 17:54:48 +0700

    btns fix

--- src/components/LayerControl.js
+++ src/components/LayerControl.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function LayerControl({ layers, toggleLayerVisibility }) {
+    const isMiddleLayerActive = layers.find(layer => layer.originalIndex === 1 && layer.visible);
+
     return (
         <div className="layer-control">
             <div className="layer-control-frame layer-control-frame_tr"></div>
@@ -10,10 +12,10 @@ export default function LayerControl({ layers, toggleLayerVisibility }) {
 
             {layers.slice().reverse().map((layer, index) => (
                 <button
-                    className={layer.visible ? '' : 'btn-active'}
+                    className={`${layer.visible ? '' : 'btn-active'} ${index === layers.length - 1 ? 'btn-bottom' : ''}`}
                     key={layer.originalIndex}
                     onClick={() => toggleLayerVisibility(layer)}
-                    disabled={index === layers.length - 1} // Disable button for the bottom layer
+                    disabled={index === layers.length - 1 || (index === layers.length - 3 && !isMiddleLayerActive)} // Disable button for the bottom layer and upper layer when middle is active
                 >
                     {layers.length - index} {/* Number buttons in reverse order */}
                 </button>
